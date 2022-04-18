9e2af3f013bdbba737534a16354c47dd88e39d27
catbob2020 <13434097858@163.com>
2022-04-18 16:36:26 +0800

    修改过的

--- multiplication.py
+++ multiplication.py
@@ -1,12 +1,8 @@
-from say_hello import *
-def main():
-    print('程序开始执行')
-    print(say_hello('孙悟空'))
-# 增加调用main()函数
-if __name__ == '__main__':
-    main()
+print('hello world')
+for i in range(9):
+    print(i)
 
-
+print('测试')
 
 
 
