9aaba4ad8e2a24c747cda03ba67c6d96be063d83
catbob2020 <13434097858@163.com>
2022-04-18 14:40:43 +0800

    测试使用

---
+++ multiplication.py
@@ -0,0 +1,44 @@
+from say_hello import *
+def main():
+    print('程序开始执行')
+    print(say_hello('孙悟空'))
+# 增加调用main()函数
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+# print('!!!!!!!!')
+#
+# for a in lst1:
+#     if a == 36 or a == 25:
+#         lst1.remove(a)
+#         lst1.insert(0,'#')
+#
+# print(lst1)
+# while '#' in lst1:
+#     lst1.remove('#')
+# print(lst1)
+# lst1=[1, 52, 24, 47, 4, 25, 21,36,5,3,23,36,2, 25, 36]
+#
+# print('!!!!!!!!')
+# b=[]
+# for a in lst1:
+#     if a != 36 and a != 25:
+#         b.append(a)
+# print(b)
+# lst1 = [1, 52, 24, 47, 4, 25, 21, 36, 5, 3, 23, 36, 2, 25, 36]
+# print(len(lst1))
+# a=lst1
+#
+# print(a,len(a))
+# for a in lst1:
+#     print(lst1.index(a))
+#     if lst1[a] == 36 or lst1[a] == 25:
+#         lst1.remove(lst1[a])
+#
+#         a += 1
+# print(lst1)
