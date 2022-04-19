c3f363498bab3e3646d03302e7802e3c3894a20e
catbob2020 <13434097858@163.com>
2022-04-19 18:59:24 +0800

    重

---
+++ multiplication.py
@@ -0,0 +1,40 @@
+print('hello world')
+for i in range(9):
+    print(i)
+
+print('测试')
+print('gmf uqw  ddf')
+print('game over')
+
+
+# print('!!!!!!!!')
+#
+# for a in lst1:
+#     if a == 36 or a == 25:
+#         lst1.remove(a)
+#         lst1.insert(0,'#')
+#
+# print(lst1)
+# while '#' in lst1:
+#     lst1.remove('#')
+# print(lst1)
+# lst1=[1, 52, 24, 47, 4, 25, 21,36,5,3,23,36,2, 25, 36]
+#
+# print('!!!!!!!!')
+# b=[]
+# for a in lst1:
+#     if a != 36 and a != 25:
+#         b.append(a)
+# print(b)
+# lst1 = [1, 52, 24, 47, 4, 25, 21, 36, 5, 3, 23, 36, 2, 25, 36]
+# print(len(lst1))
+# a=lst1
+#
+# print(a,len(a))
+# for a in lst1:
+#     print(lst1.index(a))
+#     if lst1[a] == 36 or lst1[a] == 25:
+#         lst1.remove(lst1[a])
+#
+#         a += 1
+# print(lst1)
